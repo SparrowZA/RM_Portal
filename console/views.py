5ef1a8eb197b928cdb1c775865a9c7b71ccb06ba
SparrowZA <geffroymarc@gmail.com>
2022-06-21 22:49:04 +0200

    Adding additional robustness.

--- console/views.py
+++ console/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from urllib.request import HTTPRedirectHandler
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 # Forms
@@ -16,18 +17,32 @@ from console.wrappers import login_required
 # Global usecase object.
 usecases = Usecases()
 
-def index(response):
-    if response.method == 'POST':
-        form = LoginForm(response.POST)
+def index(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
         if form.is_valid():
-            id = usecases.login(form.clean())            
-            return HttpResponseRedirect(f'/manager/{id}/console')
+            response = usecases.login(form.clean())   
+            if response.is_success():
+                rm_id = response.data['rm_id']
+                return HttpResponseRedirect(f'/manager/{rm_id}/console')
+            else:
+                data = {
+                    'response': response.to_dict(),
+                    'form': LoginForm()
+                }
+                return render(request, 'index.html', data)
+        else:
+            data = {
+                'response': {'error_message': 'Incorrect credentials.'},
+                'form': LoginForm()
+            }
+            return render(request, 'index.html', data)
     else:
         form = LoginForm()
-        return render(response, 'index.html', {'form': form})        
+        return render(request, 'index.html', {'form': form})        
 
 @login_required
-def console(response, rm_id):
+def console(request, rm_id):
     requests = usecases.get_request_list()
     form_data = {
         'request_url': 'create_request/',
@@ -36,44 +51,58 @@ def console(response, rm_id):
         'req_list':requests
     }
     
-    return render(response, 'console.html', form_data)
+    return render(request, 'console.html', form_data)
 
 @login_required
-def create_client(response, rm_id):
+def create_client(request, rm_id):
     '''
     Create new client usingthe form data.
     '''
-    if response.method == 'POST':
-        form = CreateClientForm(response.POST)
+    if request.method == 'POST':
+        form = CreateClientForm(request.POST)
         if form.is_valid():
             # Input values into form data
-            client = usecases.create_client_usecase(form.clean())
+            response = usecases.create_client_usecase(form.clean())
+            data = {
+                'console_url': f'manager/{rm_id}/console',
+                'response': response.to_dict(),
+                'form': CreateClientForm()
+            }
             
-            return HttpResponseRedirect('/console/')
+            return render(request, 'create_client.html', data)
     else:
         client_form = CreateClientForm()
         form = {
             'console_url': f'manager/{rm_id}/console',
             'form': client_form
         }
-        return render(response, 'create_client.html', form)
+        return render(request, 'create_client.html', form)
 
 @login_required
-def client_list(response, rm_id):
+def client_list(request, rm_id):
     '''Creates a client list and passes it to a html form.'''
     form = usecases.get_client_list()
     form['console_url'] = f'manager/{rm_id}/console'
-    return render(response, 'client_list.html', form)
+    return render(request, 'client_list.html', form)
 
 @login_required
-def create_request(response, rm_id):
-    if response.method == 'POST':
-        form = RequestForm(response.POST)
+def create_request(request, rm_id):
+    if request.method == 'POST':
+        form = RequestForm(request.POST)
         if form.is_valid():
-            usecases.create_request_usecase(form.clean(), rm_id)
-            return HttpResponseRedirect('')
+            response = usecases.create_request_usecase(form.clean(), rm_id)
+
+            data = {
+                'console_url': f'manager/{rm_id}/console',
+                'rm': usecases.storage.get_rm(rm_id).to_dict(),
+                'client_list': usecases.storage.get_clients(),
+                'response': response.to_dict(),
+                'form': RequestForm()
+            }
+            
+            return render(request, 'request_form.html', data)
         else:
-            return HttpResponse(response, {'message': 'The form is not valid'})
+            return HttpResponse(request, {'message': 'The form is not valid'})
     else:
         request_form = RequestForm()
         drop_down = usecases.storage.get_clients()
@@ -82,17 +111,16 @@ def create_request(response, rm_id):
             'console_url': f'manager/{rm_id}/console',
             'rm': rm.to_dict(),
             'client_list': drop_down,
-            'request': request_form
+            'form': request_form
         }
-        return render(response, 'request_form.html', form)
+        return render(request, 'request_form.html', form)
 
 def client_upload(request, client_url):
-    # Check client url is 
     if request.method == 'POST':
         form = ClientUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            usecases.upload_client_file(form.files['file'], client_url)
-
+            doc = usecases.upload_client_file(form.files['file'], client_url)
+            return HttpResponseRedirect('client_post_upload/')
     else:
         if not usecases.is_client_url_active(client_url):
             return HttpResponseRedirect('error/', {'error': 'The URL you are using is no longer active'})
@@ -109,4 +137,7 @@ def client_upload(request, client_url):
 
 def server_error(request):
     return render(request, 'error.html')
+
+def client_post_upload(request):
+    return render(request, 'client_post_upload.html')
     
